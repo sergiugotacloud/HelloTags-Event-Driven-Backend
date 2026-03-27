6b3de08d4c5a61bc567bf78cb38d1f9f2c3dd033
SERGIU GOTA <sergiugota4@gmail.com>
2026-03-27 15:38:11 +0200

    Update notification_handler.py

--- lambda/notification_handler.py
+++ lambda/notification_handler.py
@@ -1,1 +1,10 @@
+import json
 
+def lambda_handler(event, context):
+    print("EVENT RECEIVED:")
+    print(json.dumps(event))
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps("notification processed")
+    }
