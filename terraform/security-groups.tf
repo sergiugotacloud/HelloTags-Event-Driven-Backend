78e1a2eb79d828adcac14a7e4f46983b7c067281
SERGIU GOTA <sergiugota4@gmail.com>
2026-03-27 16:00:18 +0200

    Update security-groups.tf

--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,1 +1,21 @@
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main.id
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+}
