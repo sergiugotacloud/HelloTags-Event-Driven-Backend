13aea0fed1afb25ce17498a939aa48e50c1c7118
SERGIU GOTA <sergiugota4@gmail.com>
2026-03-27 16:11:14 +0200

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,39 +1,96 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+provider "aws" {
+  region = "eu-central-1"
 }
 
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name    = "hellotags-vpc"
+    Project = "hellotags"
+  }
+}
+
+# PUBLIC SUBNET
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "eu-central-1a"
   map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "hellotags-public-subnet"
+    Project = "hellotags"
+  }
 }
 
+# PRIVATE SUBNET A
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-central-1a"
+
+  tags = {
+    Name    = "hellotags-private-subnet-a"
+    Project = "hellotags"
+  }
 }
 
+# PRIVATE SUBNET B
 resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "eu-central-1b"
+
+  tags = {
+    Name    = "hellotags-private-subnet-b"
+    Project = "hellotags"
+  }
 }
 
+# INTERNET GATEWAY
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name    = "hellotags-igw"
+    Project = "hellotags"
+  }
 }
 
-resource "aws_eip" "nat" {}
+# ELASTIC IP FOR NAT
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
 
+  tags = {
+    Name    = "hellotags-nat-eip"
+    Project = "hellotags"
+  }
+}
+
+# NAT GATEWAY
 resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat.id
+  allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public.id
+
+  depends_on = [aws_internet_gateway.igw]
+
+  tags = {
+    Name    = "hellotags-nat"
+    Project = "hellotags"
+  }
 }
 
+# PUBLIC ROUTE TABLE
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name    = "hellotags-public-rt"
+    Project = "hellotags"
+  }
 }
 
 resource "aws_route" "public_internet" {
@@ -47,8 +104,14 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public.id
 }
 
+# PRIVATE ROUTE TABLE
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name    = "hellotags-private-rt"
+    Project = "hellotags"
+  }
 }
 
 resource "aws_route" "private_internet" {
