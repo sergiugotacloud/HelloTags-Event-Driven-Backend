3475c58bf01ac174e5a66a08b8c441790c545d47
SERGIU GOTA <sergiugota4@gmail.com>
2026-03-27 16:07:27 +0200

    Update eventbridge.tf

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -1,31 +1,43 @@
-resource "aws_cloudwatch_event_rule" "tap_rule" {
+resource "aws_cloudwatch_event_rule" "tap_events_rule" {
+  name        = "tap-events-rule"
+  description = "Routes tap events to downstream consumers"
+
   event_pattern = jsonencode({
-    source = ["hellotags.tap"]
+    source      = ["hellotags.tap"]
+    detail-type = ["TapEvent"]
   })
+
+  tags = {
+    Project = "hellotags"
+  }
 }
 
-resource "aws_cloudwatch_event_target" "notification" {
-  rule = aws_cloudwatch_event_rule.tap_rule.name
-  arn  = aws_lambda_function.notification_handler.arn
+# Notification Lambda target
+resource "aws_cloudwatch_event_target" "notification_target" {
+  rule      = aws_cloudwatch_event_rule.tap_events_rule.name
+  target_id = "NotificationLambda"
+  arn       = aws_lambda_function.notification_handler.arn
 }
 
-resource "aws_cloudwatch_event_target" "analytics" {
-  rule = aws_cloudwatch_event_rule.tap_rule.name
-  arn  = aws_lambda_function.analytics_handler.arn
-}
-
-resource "aws_lambda_permission" "event_notification" {
+resource "aws_lambda_permission" "allow_eventbridge_notification" {
   statement_id  = "AllowEventBridgeNotification"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.notification_handler.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.tap_rule.arn
+  source_arn    = aws_cloudwatch_event_rule.tap_events_rule.arn
 }
 
-resource "aws_lambda_permission" "event_analytics" {
+# Analytics Lambda target
+resource "aws_cloudwatch_event_target" "analytics_target" {
+  rule      = aws_cloudwatch_event_rule.tap_events_rule.name
+  target_id = "AnalyticsLambda"
+  arn       = aws_lambda_function.analytics_handler.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge_analytics" {
   statement_id  = "AllowEventBridgeAnalytics"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.analytics_handler.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.tap_rule.arn
+  source_arn    = aws_cloudwatch_event_rule.tap_events_rule.arn
 }
