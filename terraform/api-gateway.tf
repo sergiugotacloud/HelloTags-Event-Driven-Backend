adf3c8ff3d20a03078a3a959ac971e0c564a83bc
SERGIU GOTA <sergiugota4@gmail.com>
2026-03-27 16:01:27 +0200

    Update api-gateway.tf

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -1,1 +1,23 @@
+resource "aws_apigatewayv2_api" "api" {
+  name          = "hellotags-api"
+  protocol_type = "HTTP"
+}
 
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.tap_handler.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "tap" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "POST /tap"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_lambda_permission" "api" {
+  statement_id  = "AllowAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.tap_handler.function_name
+  principal     = "apigateway.amazonaws.com"
+}
