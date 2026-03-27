d91bc5c2d3c66cfc198d1c511e38636c7eeb151e
SERGIU GOTA <sergiugota4@gmail.com>
2026-03-27 16:02:29 +0200

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,1 +1,7 @@
+resource "aws_instance" "api" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_a.id
 
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+}
