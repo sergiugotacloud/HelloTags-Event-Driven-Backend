606c7a10dfd85289643001a5ba6073610fa05364
SERGIU GOTA <sergiugota4@gmail.com>
2026-03-27 16:08:11 +0200

    Update iam-lambda.tf

--- terraform/iam-lambda.tf
+++ terraform/iam-lambda.tf
@@ -1,17 +1,30 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda-role"
+  name = "hellotags-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = "sts:AssumeRole"
       Effect = "Allow"
-      Principal = { Service = "lambda.amazonaws.com" }
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
     }]
   })
+
+  tags = {
+    Project = "hellotags"
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "basic" {
+# Basic logging
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# VPC access (for private subnets)
+resource "aws_iam_role_policy_attachment" "lambda_vpc" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
 }
