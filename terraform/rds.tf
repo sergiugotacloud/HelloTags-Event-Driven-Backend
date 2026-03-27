5edfe8606d1cf8c610b781c39294850708d13989
SERGIU GOTA <sergiugota4@gmail.com>
2026-03-27 16:02:44 +0200

    Update rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,1 +1,15 @@
+resource "aws_db_subnet_group" "db" {
+  subnet_ids = [aws_subnet.private_a.id, aws_subnet.private_b.id]
+}
 
+resource "aws_db_instance" "postgres" {
+  engine               = "postgres"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = 20
+  db_subnet_group_name = aws_db_subnet_group.db.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+
+  username = "postgres"
+  password = "password123"
+  skip_final_snapshot = true
+}
