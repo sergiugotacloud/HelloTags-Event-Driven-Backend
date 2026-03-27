553f6cf049cf095afc6f94109621a8a16b6cfc1c
SERGIU GOTA <sergiugota4@gmail.com>
2026-03-27 16:06:00 +0200

    Update dynamodb.tf

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,16 +1,23 @@
 resource "aws_dynamodb_table" "tap_events" {
   name         = "tap-events"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "card_id"
-  range_key    = "timestamp"
+
+  hash_key  = "card_id"
+  range_key = "timestamp"
 
   attribute {
     name = "card_id"
     type = "S"
   }
 
+  # Use STRING for timestamp (matches Lambda behavior)
   attribute {
     name = "timestamp"
     type = "S"
+  }
+
+  tags = {
+    Name    = "hellotags-tap-events"
+    Project = "hellotags"
   }
 }
