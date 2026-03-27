4420621da4098862ddef2efd8ed97279f00f2f37
SERGIU GOTA <sergiugota4@gmail.com>
2026-03-27 16:00:34 +0200

    Update dynamodb.tf

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,1 +1,16 @@
+resource "aws_dynamodb_table" "tap_events" {
+  name         = "tap-events"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "card_id"
+  range_key    = "timestamp"
 
+  attribute {
+    name = "card_id"
+    type = "S"
+  }
+
+  attribute {
+    name = "timestamp"
+    type = "S"
+  }
+}
